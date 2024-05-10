3825b3f475c1b5257f2ccf8ce0cdb37899d8db12
kot lena <aternetas@gmail.com>
2024-05-11 00:16:16 +0300

    remove extra

--- SkyDunk/UI/Home/HomeViewModel.swift
+++ SkyDunk/UI/Home/HomeViewModel.swift
@@ -14,8 +14,6 @@ protocol HomeViewModelDelegat {
 class HomeViewModel {
     var delegate: HomeViewModelDelegat?
     
-    var lastGameVM = LastGameVM(homeTeam: "LAL", guestTeam: "BOS", homeTeamScore: 101, guestTeamScore: 116, date: Date())
-    
     func getLastGame() {
         DispatchQueue.global().asyncAfter(deadline: .now() + 3, execute: { [weak self] in
             self?.delegate?.updateLastGame(vm: LastGameVM(homeTeam: "LAL", guestTeam: "BOS", homeTeamScore: 101, guestTeamScore: 116, date: Date()))
