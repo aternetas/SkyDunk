3f10f83815e9197412cabc500f9b63e62a9468df
kot lena <aternetas@gmail.com>
2024-05-11 17:39:53 +0300

    add subviews on GameCell

--- SkyDunk/UI/Home/Cells/GameCell.swift
+++ SkyDunk/UI/Home/Cells/GameCell.swift
@@ -17,13 +17,55 @@ class GameCell: UICollectionViewCell {
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor(resource: .backgroundGray)
         view.layer.cornerRadius = 10
-        addShadows()
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    lazy private var homeTeamImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(resource: .BOS)
+        return imageView
+    }()
+    
+    lazy private var guestTeamImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(resource: .MIL)
+        return imageView
+    }()
+    
+    lazy private var gameDateLab: UILabel = {
+        let label = UILabel()
+        label.textColor = .textDark
+        label.font = UIFont(type: .extraLight, size: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "14.02.2007"
+        return label
+    }()
+    
+    lazy private var gameStartTimeLab: UILabel = {
+        let label = UILabel()
+        label.textColor = .textDark
+        label.font = UIFont(type: .extraLight, size: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "15:24"
+        return label
+    }()
+    
+    lazy private var gradientView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
         return view
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupView()
         initConstraints()
     }
     
@@ -31,14 +73,44 @@ class GameCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupView() {
+        addShadows()
+    }
+    
     private func initConstraints() {
         contentView.addSubview(containerView)
+        
+        containerView.addSubview(gradientView)
+        containerView.addSubview(homeTeamImageView)
+        containerView.addSubview(guestTeamImageView)
+        containerView.addSubview(gameDateLab)
+        containerView.addSubview(gameStartTimeLab)
         
         NSLayoutConstraint.activate([
             containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
             containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            
+            gradientView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0),
+            gradientView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0),
+            gradientView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0),
+            gradientView.widthAnchor.constraint(equalToConstant: 11),
+            
+            homeTeamImageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 24),
+            homeTeamImageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor, constant: 2),
+            homeTeamImageView.heightAnchor.constraint(equalToConstant: 30),
+            
+            guestTeamImageView.topAnchor.constraint(equalTo: homeTeamImageView.bottomAnchor, constant: 15),
+            guestTeamImageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor, constant: 2),
+            guestTeamImageView.heightAnchor.constraint(equalToConstant: 30),
+            
+            gameStartTimeLab.centerXAnchor.constraint(equalTo: containerView.centerXAnchor, constant: 2),
+            gameStartTimeLab.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
+            
+            gameDateLab.centerXAnchor.constraint(equalTo: containerView.centerXAnchor, constant: 2),
+            gameDateLab.bottomAnchor.constraint(equalTo: gameStartTimeLab.topAnchor, constant: 0)
+            
         ])
     }
 }
