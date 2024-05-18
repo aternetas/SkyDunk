a8c91543d5cde4055cf0c9e6c03bd48f26573140
kot lena <aternetas@gmail.com>
2024-05-18 14:54:00 +0300

    reorder resources, add newBetButton on GameView

--- SkyDunk/UI/Game/GameView.swift
+++ SkyDunk/UI/Game/GameView.swift
@@ -9,18 +9,28 @@ import UIKit
 
 class GameView: UIView {
     
-    lazy var gameHeaderView = {
+    lazy var gameHeaderView: GameHeaderView = {
         let view = GameHeaderView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    lazy var betsTableView = {
+    lazy var betsTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.separatorStyle = .none
         tableView.register(BetCell.self, forCellReuseIdentifier: BetCell.identifier)
         return tableView
+    }()
+    
+    lazy var newBetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(resource: .buttonBackgroundPrimary)
+        button.setImage(.plusIcon, for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 30.5
+        return button
     }()
     
     override init(frame: CGRect) {
@@ -37,6 +47,7 @@ class GameView: UIView {
     private func initConstraints() {
         addSubview(gameHeaderView)
         addSubview(betsTableView)
+        addSubview(newBetButton)
         
         NSLayoutConstraint.activate([
             gameHeaderView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
@@ -47,7 +58,12 @@ class GameView: UIView {
             betsTableView.topAnchor.constraint(equalTo: gameHeaderView.bottomAnchor, constant: 10),
             betsTableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
             betsTableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
-            betsTableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0)
+            betsTableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            
+            newBetButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            newBetButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            newBetButton.heightAnchor.constraint(equalToConstant: 61),
+            newBetButton.widthAnchor.constraint(equalToConstant: 61)
         ])
     }
 }
