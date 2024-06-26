1605adf790646ae61b00bcd4fcec70a5764b7b97
Nebo <kindersmail08@gmail.com>
2024-06-26 22:50:36 +0300

    add 2 methods

--- SkyDunk/Extensions/Date+ext.swift
+++ SkyDunk/Extensions/Date+ext.swift
@@ -15,13 +15,13 @@ fileprivate var dayMonthYearFormatter: DateFormatter = {
 
 fileprivate var hourMinuteFormatter: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateFormat = "k:mm"
+    formatter.dateFormat = "HH:mm"
     return formatter
 }()
 
 fileprivate var hourMinuteDayMonthYearFormatter: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateFormat = "k:mm d.MM.yy"
+    formatter.dateFormat = "HH:mm d.MM.yy" 
     return formatter
 }()
 
@@ -37,5 +37,57 @@ extension Date {
     
     func toHourMinuteDayMonthYear() -> String {
         hourMinuteDayMonthYearFormatter.string(from: self)
+    }
+    
+    func add(years: Int? = nil, months: Int? = nil, days: Int? = nil, hours: Int? = nil, minutes: Int? = nil) -> Date {
+        var newDate = self
+        
+        if let years = years {
+            newDate = Calendar.current.date(byAdding: .year, value: years, to: newDate)!
+        }
+        
+        if let months = months {
+            newDate = Calendar.current.date(byAdding: .month, value: months, to: newDate)!
+        }
+        
+        if let days = days {
+            newDate = Calendar.current.date(byAdding: .day, value: days, to: newDate)!
+        }
+        
+        if let hours = hours {
+            newDate = Calendar.current.date(byAdding: .hour, value: hours, to: newDate)!
+        }
+
+        if let minutes = minutes {
+            newDate = Calendar.current.date(byAdding: .minute, value: minutes, to: newDate)!
+        }
+        
+        return newDate
+    }
+    
+    /**
+     This method returns date by text representation
+     
+     The method returns an optional value since the text or format may be incorrect
+
+     - parameter text: Date text value
+     - parameter format: Date format
+     - returns: Date?
+     
+     # Example #
+    ```
+     if let date1 = Date.getByText("2024 04 12 14:30") {
+      print(date)
+     }
+     
+     if let date2 = Date.getByText("22:30", format: "HH:mm") {
+      print(date)
+     }
+     ```
+    */
+    static func getByText(_ text: String, format: String = "yyyy MM dd HH:mm") -> Date? {
+        let fmt = DateFormatter()
+        fmt.dateFormat = format
+        return fmt.date(from: text)
     }
 }
--- SkyDunk/Repositories/TMP.swift
+++ SkyDunk/Repositories/TMP.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 var tmpGames = [
-    GameDTO(homeTeam: "BOS", guestTeam: "ATL", gameDate: Date.now + 502433, homeScore: 0, guestScore: 0, betIds: []),
-    GameDTO(homeTeam: "DEN", guestTeam: "BOS", gameDate: Date.now + 5033, homeScore: 0, guestScore: 0, betIds: []),
-    GameDTO(homeTeam: "DAL", guestTeam: "ATL", gameDate: Date.now + 51033, homeScore: 0, guestScore: 0, betIds: []),
-    GameDTO(homeTeam: "BOS", guestTeam: "DAL", gameDate: Date.now + 2402, homeScore: 0, guestScore: 0, betIds: []),
-    GameDTO(homeTeam: "DEN", guestTeam: "ATL", gameDate: Date.now - 298355, homeScore: 102, guestScore: 121, betIds: []),
-    GameDTO(homeTeam: "BOS", guestTeam: "DEN", gameDate: Date.now - 247922, homeScore: 110, guestScore: 102, betIds: [])
+    GameDTO(homeTeam: "BOS", guestTeam: "ATL", gameDate: Date().add(hours: 10), homeScore: 0, guestScore: 0, betIds: []),
+    GameDTO(homeTeam: "DEN", guestTeam: "BOS", gameDate: Date().add(months: -1), homeScore: 0, guestScore: 0, betIds: []),
+    GameDTO(homeTeam: "DAL", guestTeam: "ATL", gameDate: Date().add(days: 5, minutes: 20), homeScore: 0, guestScore: 0, betIds: []),
+    GameDTO(homeTeam: "BOS", guestTeam: "DAL", gameDate: Date().add(years: -3), homeScore: 0, guestScore: 0, betIds: []),
+    GameDTO(homeTeam: "DEN", guestTeam: "ATL", gameDate: Date.getByText("2015 03 22 13:54") ?? Date(), homeScore: 102, guestScore: 121, betIds: []),
+    GameDTO(homeTeam: "BOS", guestTeam: "DEN", gameDate: Date.getByText("2015 03 22", format: "yyyy MM dd") ?? Date(), homeScore: 110, guestScore: 102, betIds: [])
 ]
