05d836c00f693241528edf25440a026c8c36a503
kot lena <aternetas@gmail.com>
2024-07-01 15:24:17 +0300

    refactor editBet()

--- SkyDunk/Repositories/BetRepository.swift
+++ SkyDunk/Repositories/BetRepository.swift
@@ -22,20 +22,18 @@ class BetRepository {
     }
     
     func editBet(id: String, description: String? = nil, isSuccess: Bool? = nil) {
-        DispatchQueue.global().asyncAfter(deadline: .now() + 0.3, execute: { [weak self] in
-            let dto = self?.findBetById(id: id)
-            let index = tmpBets.firstIndex(where: { $0.id == id })
-            
-            if let dto = dto, let index = index {
-                let editDto = BetDTO(id: id,
-                                     gameId: dto.gameId,
-                                     description: description ?? dto.description,
-                                     betCreated: dto.betCreated,
-                                     amount: dto.amount,
-                                     coefficient: dto.coefficient,
-                                     betOn: dto.betOn,
-                                     isSuccess: isSuccess ?? dto.isSuccess)
-                tmpBets[index] = editDto
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.3, execute: {
+            tmpBets.enumerated().forEach { index, dto in
+                if dto.id == id {
+                    tmpBets[index] = BetDTO(id: id,
+                                            gameId: dto.gameId,
+                                            description: description ?? dto.description,
+                                            betCreated: dto.betCreated,
+                                            amount: dto.amount,
+                                            coefficient: dto.coefficient,
+                                            betOn: dto.betOn,
+                                            isSuccess: isSuccess ?? dto.isSuccess)
+                }
             }
         })
     }
