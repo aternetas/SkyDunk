a4ffb5d3e6089a27badbef4f8938c76c7a6df641
kot lena <aternetas@gmail.com>
2024-07-11 14:59:02 +0300

    PastGamesScreen: add separatorView

--- SkyDunk/UI/PastGames/PastGamesView.swift
+++ SkyDunk/UI/PastGames/PastGamesView.swift
@@ -16,6 +16,12 @@ class PastGamesView: UIView {
         return tableView
     }()
     
+    private lazy var separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .backgroundGray3
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
@@ -28,10 +34,16 @@ class PastGamesView: UIView {
     }
     
     public func initConstraints() {
-        addSubviewsAndAutolayout([pastGamesTableView])
+        addSubviewsAndAutolayout([separatorView,
+                                  pastGamesTableView])
         
         NSLayoutConstraint.activate([
-            pastGamesTableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 277),
+            separatorView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 252),
+            separatorView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 13),
+            separatorView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -13),
+            separatorView.heightAnchor.constraint(equalToConstant: 1),
+            
+            pastGamesTableView.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 17),
             pastGamesTableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
             pastGamesTableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
             pastGamesTableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
